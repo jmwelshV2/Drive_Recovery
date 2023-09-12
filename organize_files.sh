6e59d49df8d92f4aafdafaa7284ccdfd5e04787e
L'Brain James V2 <143547261+jmwelshV2@users.noreply.github.com>
2023-09-11 20:22:09 -0400

    Update organize_files.sh

--- organize_files.sh
+++ organize_files.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
-# Directory containing recovered files
-RECOVERED_DIR="/path/to/recovered/files"
+# Directories containing recovered files
+RECOVERY_DIRS=("/mnt/recovery1" "/mnt/recovery2")
 
 # Directory containing metadata files
 METADATA_DIR="./metadata"
 
-find "$METADATA_DIR" -type f | while read -r meta_file; do
-    # Logic to read metadata and move files based on criteria
-    # This is a simple demonstration where it reads creation date to organize files.
-    
-    original_file="$RECOVERED_DIR/$(basename "$meta_file" .meta)"
-    creation_date=$(grep "Create Date" "$meta_file" | awk -F': ' '{print $2}')
+# Directory to store organized files
+ORGANIZED_DIR="./organized_files"
 
-    if [[ ! -z "$creation_date" ]]; then
-        # Creating a directory based on creation date
-        target_dir="$RECOVERED_DIR/organized/$(date -d "$creation_date" +'%Y-%m-%d')"
-        mkdir -p "$target_dir"
-        
-        # Moving the file to the target directory
-        mv "$original_file" "$target_dir/"
+mkdir -p "$ORGANIZED_DIR"
+
+find "$METADATA_DIR" -type f | while read -r meta_file; do
+  # Logic to read metadata and move files based on criteria
+  # This is a simple demonstration where it reads creation date to organize files.
+  
+  original_file=""
+  for RECOVERY_DIR in "${RECOVERY_DIRS[@]}"; do
+    possible_file="$RECOVERY_DIR/$(basename "$meta_file" .meta)"
+    if [[ -f "$possible_file" ]]; then
+      original_file="$possible_file"
+      break
     fi
+  done
+
+  if [[ -z "$original_file" ]]; then
+    echo "Original file for $meta_file not found"
+    continue
+  fi
+
+  creation_date=$(grep "Create Date" "$meta_file" | awk -F': ' '{print $2}')
+
+  if [[ ! -z "$creation_date" ]]; then
+    # Creating a directory based on creation date
+    target_dir="$ORGANIZED_DIR/$(date -d "$creation_date" +'%Y-%m-%d')"
+    mkdir -p "$target_dir"
+    
+    # Moving the file to the target directory
+    mv "$original_file" "$target_dir/"
+  fi
 done
