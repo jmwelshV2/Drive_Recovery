bfc3b3833cb44e30f67fa35c3e0697ea82cbc2ae
L'Brain James V2 <143547261+jmwelshV2@users.noreply.github.com>
2023-09-11 20:21:05 -0400

    Update extract_metadata.sh

--- extract_metadata.sh
+++ extract_metadata.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Directory containing recovered files
-RECOVERED_DIR="/path/to/recovered/files"
+# Directories containing recovered files
+RECOVERY_DIRS=("/mnt/recovery1" "/mnt/recovery2")
 
 # Directory to store metadata files
 METADATA_DIR="./metadata"
 
 mkdir -p "$METADATA_DIR"
 
-find "$RECOVERED_DIR" -type f | while read -r file; do
-    # Generate a metadata file for each recovered file
-    metadata_file="$METADATA_DIR/$(basename "$file").meta"
-    exiftool -a -u -g1 "$file" > "$metadata_file"
+for RECOVERY_DIR in "${RECOVERY_DIRS[@]}"; do
+  if [[ -d "$RECOVERY_DIR" ]]; then
+    find "$RECOVERY_DIR" -type f | while read -r file; do
+      # Generate a metadata file for each recovered file
+      metadata_file="$METADATA_DIR/$(basename "$file").meta"
+      exiftool -a -u -g1 "$file" > "$metadata_file"
+    done
+  else
+    echo "Directory $RECOVERY_DIR does not exist"
+  fi
 done
